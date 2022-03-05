8c9e4b7eade66e914edade67609a0118999e751e
Taj09 <92683801+Taj09@users.noreply.github.com>
2022-03-05 21:23:18 +0530

    Create Client.java

---
+++ GoShopping/com.tns.client/Client.java
@@ -0,0 +1,34 @@
+package com.tns.client;
+
+import com.tns.application.*;
+//import com.tns.framework.*;
+
+public class Client 
+{
+	public static void main(String[] args) 
+	{
+		
+		GSShopFactory gssfactory = new GSShopFactory();
+		gssfactory.getNewPrimeAccount(101, "Avatar", 1000, true);
+		gssfactory.getNewNormalAccount(201, "John", 1000, 50);
+				
+		GSPrimeAcc gsprime = new GSPrimeAcc();
+		gsprime.getAccNm();
+		gsprime.getAccNo();
+		gsprime.getCharges();
+		
+		gsprime.bookProduct(1000);
+		
+		gsprime.toString();
+				
+		GSNormalAcc gsnormal = new GSNormalAcc(); 
+		gsnormal.getAccNm();
+		gsnormal.getAccNo();
+		gsnormal.getCharges();
+		gsnormal.getDeliveryCharge();
+		
+		gsnormal.bookProduct(1000);
+		
+		gsnormal.toString();
+	}
+}
