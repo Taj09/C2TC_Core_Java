59dc026c94faa6cb05933d3bcdb2100c7d2be779
Taj09 <92683801+Taj09@users.noreply.github.com>
2022-03-05 21:18:41 +0530

    Add files via upload

---
+++ GoShopping/com.tns.application/GSPrimeAcc.java
@@ -0,0 +1,29 @@
+package com.tns.application;
+
+import com.tns.framework.PrimeAcc;
+
+public class GSPrimeAcc extends PrimeAcc 
+{
+
+	private static final float charges=0; 
+	
+	
+	public float getCharges() {
+		return charges;
+	}
+
+	public GSPrimeAcc() 
+	{
+		super(accNo, accNm, charges, isPrime);
+	}
+	
+	public void bookProduct(float charges)
+	{
+		System.out.println("Dear Prime User, Your Product Charges are: "+charges);
+	}
+
+	@Override
+	public String toString() {
+		return "GSPrimeAcc []";
+	}
+}
